2f3cc7ff6872db7e1e7e324521a447ae512a49a0
Logan Hodgins <loganhodginsofficial@gmail.com>
2023-01-02 23:00:58 -0600

    Added tut files

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -4,6 +4,17 @@ from django.urls import reverse
 from django.utils.text import slugify
 
 # Create your models here.
+class Country(models.Model):
+  name = models.CharField(max_length=80)
+  code = models.CharField(max_length=2)
+
+  def __str__(self):
+    return self.name
+
+  class Meta:
+    verbose_name_plural = 'Countries'
+
+
 class Address(models.Model):
   street = models.CharField(max_length=80)
   postal_code = models.CharField(max_length=5)
@@ -14,6 +25,7 @@ class Address(models.Model):
 
   class Meta:
     verbose_name_plural = 'Address Entries'
+
 
 class Author(models.Model):
   first_name = models.CharField(max_length=100)
@@ -26,12 +38,14 @@ class Author(models.Model):
   def __str__(self):
     return self.full_name()
 
+
 class Book(models.Model):
   title = models.CharField(max_length=50)
   rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
   author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, related_name='books')
   is_bestselling = models.BooleanField(default=False)
   slug = models.SlugField(default="", blank=True, null=False, db_index=True)
+  published_countries = models.ManyToManyField(Country)
 
   def get_absolute_url(self):
     return reverse("book-detail", args=[self.slug])
